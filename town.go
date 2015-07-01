9bf88aecb8e22a429848ceea519d65315db10b0b
Kiril Menshikov <kiril@kiril.me>
2015-07-02 00:24:10 +0300

    add dynamic scale

--- town.go
+++ town.go
@@ -379,7 +379,7 @@ func (t *Town) CreateContainers(checkChanged bool) {
       time.Sleep(1000 * time.Millisecond)
     } else if len(node.Container.Exist) <= node.Container.Scale {
       log.Println(node.Container.Name, "  image: ", node.Container.Image, "  ", node.Container.Scale)
-      var create [ node.Container.Scale ]bool
+      var create =  make([]bool, node.Container.Scale)
       for i := 0; i < node.Container.Scale; i++ {
         create[i] = true
       }
